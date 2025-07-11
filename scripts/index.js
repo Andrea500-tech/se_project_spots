dfe6df78568f4056e076c0ca1a766f16407000d7
Andrea <andrealackson@gmail.com>
2025-07-10 20:03:00 -0500

    feat: add social media app with profile editing, post creation, and modal interactions

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,9 @@
 const initialCards = [
   {
+    name: "Golden Gate Bridge",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/7-photo-by-griffin-wooldridge-from-pexels.jpg",
+  },
+  {
     name: "Val Thorens",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/1-photo-by-moritz-feldmann-from-pexels.jpg",
   },
@@ -24,6 +28,7 @@ const initialCards = [
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg",
   },
 ];
+// Profile elements
 const profileName = document.querySelector(".profile__name");
 const profileDescription = document.querySelector(".profile__description");
 const editProfileBtn = document.querySelector(".profile__edit-btn");
@@ -42,6 +47,12 @@ const newPostCloseBtn = newPostModal.querySelector(".modal__close-btn");
 const newPostForm = newPostModal.querySelector(".modal__form");
 const imageLinkInput = newPostModal.querySelector("#card-image-input");
 const imageCaptionInput = newPostModal.querySelector("#card-caption-input");
+// Preview modal elements
+const previewModal = document.querySelector("#preview-modal");
+const previewCloseBtn = previewModal.querySelector(".modal__close-btn");
+const previewImage = previewModal.querySelector(".modal__preview-image");
+const previewCaption = previewModal.querySelector(".modal__caption");
+
 // Modal functionality
 function openModal(modal) {
   modal.classList.add("modal__is-opened");
@@ -66,6 +77,10 @@ newPostBtn.addEventListener("click", function () {
 newPostCloseBtn.addEventListener("click", function () {
   closeModal(newPostModal);
 });
+// close the preview modal
+previewCloseBtn.addEventListener("click", function () {
+  closeModal(previewModal);
+});
 // Handle the edit profile form submission
 
 function handleEditProfileFormSubmit(event) {
@@ -78,19 +93,50 @@ editProfileForm.addEventListener("submit", handleEditProfileFormSubmit);
 // Handle the new post form submission
 function handleNewPostFormSubmit(event) {
   event.preventDefault();
-  console.log("New post added:", {
-    imageLink: imageLinkInput.value,
-    caption: imageCaptionInput.value,
-  });
+  const inputValues = {
+    name: imageCaptionInput.value,
+    link: imageLinkInput.value,
+  };
+  const cardElement = getCardElement(inputValues);
+  cardsContainer.prepend(cardElement);
   closeModal(newPostModal);
   newPostForm.reset();
 }
 
 newPostForm.addEventListener("submit", handleNewPostFormSubmit);
 
-initialCards.forEach(function (card){
-  console.log("Card added:", {
-    name: card.name,
-    link: card.link,
+// Card functionality
+const cardsContainer = document.querySelector(".cards__list");
+const cardTemplate = document.querySelector("#card-template").content;
+function getCardElement(data) {
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+  const cardImage = cardElement.querySelector(".card__image");
+  const cardTitleEl = cardElement.querySelector(".card__title");
+  cardImage.src = data.link;
+  cardImage.alt = data.name;
+  cardTitleEl.textContent = data.name;
+  const cardLikeBtn = cardElement.querySelector(".card__like-btn");
+  cardLikeBtn.addEventListener("click", () => {
+    cardLikeBtn.classList.toggle("card__like-btn_active");
   });
-})
+  const cardDeleteBtn = cardElement.querySelector(".card__delete-btn");
+  cardDeleteBtn.addEventListener("click", () => {
+    cardElement.remove();
+    cardElement = null;
+  });
+  cardImage.addEventListener("click", () => {
+    previewImage.src = data.link;
+    previewImage.alt = data.name;
+    previewCaption.textContent = data.name;
+    openModal(previewModal);
+  });
+
+  return cardElement;
+}
+
+// Initial rendering of cards
+
+initialCards.forEach((item) => {
+  const cardElement = getCardElement(item);
+  cardsContainer.append(cardElement);
+});
